0d4d79756aecf8b05a74c30ea4f10f763d8aae68
souvikjs01 <souvik741156@gmail.com>
2024-07-13 21:35:21 +0530

    fixed6

--- middleware.ts
+++ middleware.ts
@@ -18,6 +18,5 @@ export default async function middleware (req: NextRequest){
 }
 
 export const config = {
-    // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
-    matcher: ["/middleware"]
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
 }
