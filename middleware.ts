2ff381b3b65567919a72e6c523db2fcd0cee064f
souvikjs01 <souvik741156@gmail.com>
2024-07-13 20:22:22 +0530

    fixed4

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,12 @@ import type { NextRequest } from "next/server";
 import { auth } from "./auth";
 
 const protectedRoutes = ["/middleware"];
+
 export default async function middleware (req: NextRequest){
     const session = await auth();
     const isProtected = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route));
     if(!session && isProtected){
-        const absoluteURL = new URL("/", req.nextUrl.origin);
-        return NextResponse.redirect(absoluteURL.toString());
+        return NextResponse.redirect('/');
     }
     return NextResponse.next();
 }
